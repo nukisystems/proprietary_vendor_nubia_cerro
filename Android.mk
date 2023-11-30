aa331f0ecdd30c70d3dabbc46907b12876647237
LuK1337 <priv.luk@gmail.com>
2023-11-30 20:47:49 +0100

    cerro: Setup firmware extraction
    Change-Id: I693e59abbfcec4f016de324517ad0e24ef565c65

--- Android.mk
+++ Android.mk
@@ -3,3 +3,29 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),cerro)
+
+$(call add-radio-file-sha1-checked,radio/aop.img,bdc73ecff57e8485a276e8daeef3144bd9651410)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,72ee736c005ac9ddf49dd8ddeacab7e90314b3de)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,ae605ff97eb7b3a04a5a348d2e65861305ff9e0d)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,64ab8443096e45bc566cab90413a662054c127aa)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,579bfe4c163b0b9e61c3f146c8d77e45be5246eb)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,d3e9eb3b437ae239bcb7acf992b940e51c3c0cd3)
+$(call add-radio-file-sha1-checked,radio/dsp.img,999a6d24122dd2468055d205a48d82d06a07436d)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,505d55ccc531d69725867d7454eeab3389e760a6)
+$(call add-radio-file-sha1-checked,radio/hyp.img,cb450000c341189c1884603e2a930755d0a14236)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,9e0404e40549377e5c19b7ba471f7b4e65b6e7af)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,dc6beb5a1ed30851847e227e5c8912b4beda14f1)
+$(call add-radio-file-sha1-checked,radio/modem.img,b1695456623c12b30553f28439b1144f961833a8)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,9dd09e2c1e2f08c80ac65de161aa499e556ad26c)
+$(call add-radio-file-sha1-checked,radio/shrm.img,c2b7c3c717f2eb980cf5349bb0e8d624ab447ea8)
+$(call add-radio-file-sha1-checked,radio/spuservice.img,170b4470d7b2740ff2f6daf8e3e51d01b279dc37)
+$(call add-radio-file-sha1-checked,radio/tz.img,453f0d0ef1981162364f96e1dbf83df13421de57)
+$(call add-radio-file-sha1-checked,radio/uefi.img,005383b70de68f5dd9aa692635fd2d5255f4b4f3)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,e8c5c8ce46ca18172121a2558f05ec01feb42162)
+$(call add-radio-file-sha1-checked,radio/xbl.img,1615746e3dbe61d9d74c140cf5fa771d3a24ec1e)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,831a684425c7c87df14fb88424ace16cf655b16d)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,6cb4d1e30d53a3febe14f7808cae3ffe81a5070a)
+
+endif
